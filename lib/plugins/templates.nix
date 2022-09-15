44e9fc3a57938bacb089dfda83031a0e52bf4cb2
Yannik Sander <yannik@floxdev.com>
2022-09-15 21:21:58 +0200

    Feature/simple templates (#79)
    * Allow templates to be defined by a .template file
    
    * Format using alejandra
    
    * Fix tested path
    
    * Make template marker a nix file

--- lib/plugins/templates.nix
+++ lib/plugins/templates.nix
@@ -1,14 +1,28 @@
 # capacitor API
-{lib,...}:
+{lib, ...}:
 # user API
-{ dir ? "templates" }:
+{dir ? "templates"}:
 # Plugin API
-{ context, capacitate, ... }:
 {
-    templates = builtins.mapAttrs
-          (k: v: {
-            path = v.path;
-            description = (import (v.path + "/flake.nix")).description or "no description provided in ${v.path}/flake.nix";
-          })
-          (lib.capacitor.dirToAttrs  (context.self + "/${dir}") { });
+  context,
+  capacitate,
+  ...
+}: {
+  templates =
+    builtins.mapAttrs
+    (
+      k: v:
+        if builtins.pathExists (v.path + "/flake.nix")
+        then {
+          path = v.path;
+          description = (import (v.path + "/flake.nix")).description or "no description provided in ${v.path}/flake.nix";
+        }
+        else if builtins.pathExists (v.path + "/template.nix")
+        then {
+          path = v.path + "/files";
+          description = (import (v.path + "/template.nix")).description or "no description provided in ${v.path}/template.nix";
+        }
+        else throw "Invalid template directory: ${v.path}"
+    )
+    (lib.capacitor.dirToAttrs (context.self + "/${dir}") {});
 }
